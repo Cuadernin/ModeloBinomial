ef83309a71efe55a9bf3d509c37d6ab8e7f2e0fb
Cuadernin <eguerreroc1700@alumno.ipn.mx>
2021-05-18 22:14:18 -0500

    Version 1

---
+++ ModBinomial1.py
@@ -0,0 +1,52 @@
+from math import exp
+from tree import arbol
+
+def binomial(S,K,T,r,u,d,n,put=False,am=True):
+    """
+    Argumentos:
+        S (número): So ---> Valor de la acción
+        K (número): Precio de ejercicio
+        T (número): Total de periodos
+        r (número): TLR ---> Tasa libre de riesgo
+        u (número): Probabilidad de que suba
+        d (número): Probabilidad de que baje
+        n (número): Total de periodos
+    Returns:
+        [lista]: [valores]
+    """
+    t=T/n  # tiempo entre cada periodo 
+    p=(exp(r*t)-d)/(u-d)    # -----> probbabilidad p
+    lista=[];valores=[]
+    pos=-1 if put==True else 1
+    for m in range(n+1):
+        st=S*(u**(m))*(d**(n-m)) 
+        valores.append(max(pos*(st-K),0))
+        for i in range(m+1):  # Aqui si necesitamos todos los valores para cada nodo
+            st=S*(u**(m-i))*(d**i)
+            if n<10: lista.append(f's0*u^{m-i}(d^{i})={round(st,2)}')
+            elif n>=10: lista.append(round(st,2))
+            elif n>=38: lista.append(round(st,1))
+    for k in range(n-1,-1,-1): # conteo que va desde N-1,N-2,N-3...0 
+        for i in range(k+1):
+            if am: valores[i]=max(pos*(S*u**i*d**(k-i)-K),exp(-r*t)*(p*valores[i+1]+(1-p)*valores[i]))
+            else: valores[i]=exp(-r*t)*(p*valores[i+1]+(1-p)*valores[i]) # valores de la opcion segun el periodo
+    if put: 
+        put=valores[0]
+        call=put-K*exp(-r*T)+S
+        if am:
+            print(f"Valor del call AMERICANO ==== {call}")
+            print(f"Valor del put AMERICANO ==== {put}")
+        else:
+            print(f"Paridad call-put ------------------------> {put+S} = {call+K*exp(-r*T)}")
+            print(f"Valor del call ==== {call}")
+            print(f"Valor del put  ==== {put}")
+    else:  
+        call=valores[0]
+        put=call+K*exp(-r*T)-S
+        print(f"Paridad call-put ------------------------> {put+S} = {call+K*exp(-r*T)}")
+        print(f"Valor del call ==== {call}")
+        print(f"Valor del put  ==== {put}")
+    return lista
+N=20;S=50;K=51;T=6/12;r=0.05;u=1.06;d=0.95 # Ejemplo
+fn=binomial(S,K,T,r,u,d,N,put=False,am=False)
+arbol(N,fn)
---
+++ tree.py
@@ -0,0 +1,116 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from graphviz import Source
+
+def arbol(n,lista):
+    """
+    Diagrama para visualizar los precios
+
+    Args:
+        n: total de periodos
+        lista: precios
+    """
+    G=nx.Graph()
+    for i in range(n+1):
+        for k in range(1,i+2):
+            if i<n:
+                G.add_edge((i,k),(i+1,k))
+                G.add_edge((i,k),(i+1,k+1))
+    posG,dic={},{}
+    for i,node in enumerate(G.nodes()):
+        posG[node]=[node[0],n+2+node[0]-2*node[1]] 
+        dic[node]=lista[i]
+    if n<=10:
+        plt.figure(1,figsize=(20,20))
+        nx.draw(G,pos=posG,node_size=150,alpha=0.6)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=15)
+        xmax=n
+        ymax=n*2
+        plt.suptitle(f"Gráfico de {n} periodos")
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.show()
+    elif n>10 and n<15:
+        plt.figure(1,figsize=(20,20))
+        nx.draw(G,pos=posG,node_size=70)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=15)
+        xmax=n
+        ymax=n*2
+        plt.suptitle(f"Gráfico de {n} periodos")
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.show()
+    elif n>=15 and n<29:
+        plt.figure(1,figsize=(20,20))
+        nx.draw(G,pos=posG,node_size=70,node_color='pink')
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=10,font_weight='bold')
+        xmax=n
+        ymax=n*2
+        plt.suptitle(f"Gráfico de {n} periodos")
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.show()
+    elif n>=29 and n<48:
+        plt.figure(1,figsize=(21,21))
+        nx.draw(G,pos=posG,node_size=30,node_color='yellow')
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=7,font_weight='bold')
+        xmax=n
+        ymax=n*2
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.suptitle(f"Gráfico de {n} periodos")
+        plt.savefig("nodos.svg",dpi=1000)
+    elif n>=48 and n<65:
+        plt.figure(1,figsize=(21,21))
+        nx.draw(G,pos=posG,node_size=40,node_color='orange',alpha=0.6)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=5,font_weight='bold')
+        xmax=n
+        ymax=n*2
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.suptitle(f"Gráfico de {n} periodos")
+        plt.savefig("nodos.svg",dpi=1000)
+    elif n>=65 and n<76:
+        plt.figure(1,figsize=(21,21))
+        nx.draw(G,pos=posG,node_size=30,node_color='orange',alpha=0.8)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=5,font_weight='bold')
+        xmax=n
+        ymax=n*2
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.suptitle(f"Gráfico de {n} periodos")
+        #plt.savefig("nodos.pdf",bbox_inches="tight")
+        plt.savefig("nodos.svg",dpi=1000)
+    elif n>=76 and n<86:
+        plt.figure(1,figsize=(21,21))
+        nx.draw(G,pos=posG,node_size=20,node_color='orange',alpha=0.8)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=4,font_weight='bold')
+        #xmax=max(xx for xx, yy in posG.values())
+        xmax=n
+        ymax=n*2
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.suptitle(f"Gráfico de {n} periodos")
+        #plt.savefig("nodos.pdf",bbox_inches="tight")
+        plt.savefig("nodos.svg",dpi=1000)
+    elif n>=86 and n<100:
+        plt.figure(1,figsize=(21,21))
+        nx.draw(G,pos=posG,node_size=10,node_color='orange',alpha=0.7)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=3,font_weight='bold')
+        xmax=n
+        ymax=n*2
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        #plt.savefig("nodos.pdf",bbox_inches="tight")
+        plt.savefig("nodos.svg",dpi=1000)
+    elif n>=100:
+        plt.figure(1,figsize=(21,21))
+        nx.draw(G,pos=posG,node_size=3.1,node_color='orange',alpha=0.6)
+        nx.draw_networkx_labels(G,posG,labels=dic,font_size=3.1,font_weight='bold')
+        xmax=n
+        ymax=n*2
+        plt.xlim(-1,xmax+1)
+        plt.ylim(-1,ymax+1)
+        plt.suptitle(f"Gráfico de {n} periodos")
+        #plt.savefig("nodos.pdf",bbox_inches="tight")
+        plt.savefig("nodos.svg",dpi=1000)
